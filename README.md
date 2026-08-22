e3866c05696ffbce377e824d5334eb866011140b
rarfel <faelmvitor@gmail.com>
2026-08-22 03:03:51 +0000

    Added some boilerplate code for window to show up

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # SDL3 Window
 Simple SDL3 window written in c++, a template for future projects.  
-It contains two bash scripts, one for the first build `setup.sh` and the other to run `make.sh`, simply type in the terminal `sh setup.sh` to create the build directory, and after building `sh make.sh` whenever you want to run it.
+It contains two bash scripts, one for the first build `setup.sh` and the other to run `make.sh`, simply type in the terminal `sh setup.sh` to create the build directory, and after building `sh make.sh` whenever you want to run it.  
+The SDL has to get cloned from <a href="https://wiki.libsdl.org/SDL3/SourceCode">source</a> to work, simply delete the SDL directory in vendor and clone it there again.
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,73 @@
 #include "SDL3/SDL.h"
+#include "SDL3/SDL_main.h"
 
-int main()
+struct SDLWindowState
 {
-  SDL_Log("%s", "Hello from SDL!");
+  SDL_Window *window;
+  SDL_Renderer *renderer;
+};
+
+void cleanUp(SDLWindowState &state);
+
+int main(int argc, char *argv[])
+{
+  SDLWindowState state;
+
+  if(!SDL_Init(SDL_INIT_VIDEO))
+  {
+    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error when initializing SDL3", nullptr);
+    return 1;
+  }
+
+  // init window
+  int width = 800;
+  int height = 600;
+  state.window = SDL_CreateWindow("SDL3Window", width, height,0);
+
+  if(!state.window)
+  {
+    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error when creating window", nullptr);
+    cleanUp(state);
+    return 1;
+  }
+
+  // init renderer
+  state.renderer = SDL_CreateRenderer(state.window, nullptr);
+  if(!state.renderer)
+  {
+    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error when creating renderer", state.window);
+    cleanUp(state);
+    return 1;
+  }
+
+  // game loop
+  bool running = true;
+  while(running)
+  {
+    SDL_Event event { 0 };
+    while(SDL_PollEvent(&event))
+    {
+      switch (event.type)
+      {
+        case SDL_EVENT_QUIT:
+        {
+          running = false;
+          break;
+        }
+      }
+    }
+    // Drawing commands
+    SDL_SetRenderDrawColor(state.renderer, 255, 255, 255, 255);
+    SDL_RenderClear(state.renderer);
+    //swap buffers and show to screen
+    SDL_RenderPresent(state.renderer);
+  }
+  cleanUp(state);
+}
+
+void cleanUp(SDLWindowState &state)
+{
+  SDL_DestroyRenderer(state.renderer);
+  SDL_DestroyWindow(state.window);
+  SDL_Quit();
 }
